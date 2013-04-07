20b59e7cffaff9cd55c51c82812fbcff7eb6c30a
Tyler Barth <divergio@gmail.com>
2013-04-07 16:49:27 +0800

    Initial Commit

---
+++ DemoAppForCloudFaker/TJBAppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  TJBAppDelegate.h
+//  DemoAppForCloudFaker
+//
+//  Created by Tyler Barth on 2013-04-07.
+//  Copyright (c) 2013年 Tyler Barth. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface TJBAppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ DemoAppForCloudFaker/main.m
@@ -0,0 +1,18 @@
+//
+//  main.m
+//  DemoAppForCloudFaker
+//
+//  Created by Tyler Barth on 2013-04-07.
+//  Copyright (c) 2013年 Tyler Barth. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+#import "TJBAppDelegate.h"
+
+int main(int argc, char *argv[])
+{
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([TJBAppDelegate class]));
+    }
+}
